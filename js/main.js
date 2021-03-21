81249c2493071bd85d1c3f1db59fb14f6d0ce41f
aquariusdat <41604428+aquariusdat@users.noreply.github.com>
2021-03-21 10:04:33 +0700

    update code

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
+$(document).ready(function () {
+    TweenMax.staggerFrom(
+        $('.welcome .text__welcome'),
+        0.8,
+        { x: 200, opacity: 0 },
+        0.4
+    );
+})
+
+
+
 var input = document.querySelectorAll('.footer__content .submit .col__item input');
 
-document.querySelectorAll('.footer__content .submit .col__item input').forEach(function(e){
-    e.addEventListener("focus", function(){
-        $(this).siblings('p').each(function(){
-                console.log(this)
-                if(!this.classList.contains('focus'))
-                {
-                    this.classList.add('focus');
-                }
-                
-            
+document.querySelectorAll('.footer__content .submit .col__item input').forEach(function (e) {
+    e.addEventListener("focus", function () {
+        $(this).siblings('p').each(function () {
+            console.log(this)
+            if (!this.classList.contains('focus')) {
+                this.classList.add('focus');
+            }
+
+
         });
     })
-    e.addEventListener("blur", function(){
-        $(this).siblings('p').each(function(){
-                console.log(this)
-                if(this.classList.contains('focus'))
-                {
-                    this.classList.remove('focus');
-                }
-                
-            
+    e.addEventListener("blur", function () {
+        $(this).siblings('p').each(function () {
+            console.log(this)
+            if (this.classList.contains('focus')) {
+                this.classList.remove('focus');
+            }
+
+
         });
     })
 })
